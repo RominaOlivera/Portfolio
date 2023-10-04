a0635d61a625007d0714b55c78a334efe19bcf7b
RominaOlivera <89470788+RominaOlivera@users.noreply.github.com>
2023-10-04 15:01:07 -0300

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,9 +5,11 @@ import { BsGithub } from 'react-icons/bs';
 import{CgWebsite } from "react-icons/cg"
 import poke from "../assets/Poke.jpg"
 import clinicaAnimalia from "../assets/clinicaAnimalia.jpeg"
+import DemoClinica from "../assets/clinicaAnimalia.mp4"
 import DemoFree from "../assets/freeStyle - .mp4"
 import DemoPoke from"../assets/poke-app.mp4"
 import FreeStyle from "../assets/freestyle.jpeg"
+import grafo from "../assets/visualizadordegrafo.jpeg"
 
 import "./productList.css";
 
@@ -109,7 +111,7 @@ const Work = () => {
                 Clinica Animalia
               </span>
               <div className='pt-8 text-center'>
-                <a href="#" target="_blank">
+                <a href={DemoClinica} target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
@@ -123,29 +125,29 @@ const Work = () => {
             </div>
           </div>
             {/* Grid Item */}
-          {/* <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+          <div
+            style={{ backgroundImage: `url(${grafo})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
-            Hover Effects 
+            {/* Hover Effects  */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Visualizador de grafo
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://visualizadordegrafo.vercel.app' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/RominaOlivera/visualizador-recorrido-grafo' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
               </div>
             </div>
-          </div> */}
+          </div>
            {/* <div
             style={{ backgroundImage: `url(${realEstate})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
